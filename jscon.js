24fcb0ff98a35d4caca06c03500039e6de33e090
Fett <fyter@live.cn>
2021-03-11 23:24:18 +0800

    Optimized for mobile devices

--- jscon.js
+++ jscon.js
@@ -1,5 +1,5 @@
 
-// Copyright [yyyy] [name of copyright owner]
+// Copyright 2021 fyter.cn/fett
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -15,6 +15,7 @@
 class con{
 	constructor(opt){
 		if(window.con_impl)return
+        this.ver="v1.2"
 		window.con_impl=this
 		var dc=function(name,option){
 			var t=document.createElement(name)
@@ -43,7 +44,7 @@ class con{
 		var input=this.wnd=dc("input",{style:"width:100%;border:0;background-color:#0000;color:#fff;flex-grow:1;outline:none;font:10px sans-serif"})
 		var cmd=this.wnd=dc("div",{children:[note,tip,input],style:"display:flex"})
 		var main=this.main=this.wnd=dc("div",{children:[wnd,cmd,ctrl],style:"position:fixed;bottom:0;min-height:20px;text-shadow:#000 1px 1px;right:0;left:0;color:white;font-size:small"})
-        if(opt.bgcolor)main.style.backgroundColor=opt.bgcolor
+        if(opt&&opt.bgcolor)main.style.backgroundColor=opt.bgcolor
         input.setAttribute("spellcheck","false")
 		tip.style.display="none"
 		document.body.appendChild(main)
@@ -102,7 +103,7 @@ class con{
 			tip.style.display="none"
 			tipsel=null
 			if(input.value=="")return
-			var m=input.value.match(/([$_\w]+[$_\w\d]*\.?)*$/)
+			var m=input.value.match(/([$_\w]+[$_\w\d]*\.?)+$/)
 			if(!m)return
 			tip.innerHTML=""
 			var frags=m[0].split(".")
@@ -141,6 +142,12 @@ class con{
             if(_.keyCode==9){event.preventDefault()}
 			if(tipsel){
 				switch(_.keyCode){
+                    case 27://esc
+                    case 8://backspace
+                        tipsel.parentNode.style.display="none"
+                        tipsel=null
+						event.preventDefault()
+                        break
 					case 38:
 						if(tipsel.previousElementSibling){
 							tipsel.style.backgroundColor="inherit"
@@ -167,7 +174,25 @@ class con{
                         tipsel.click()
                         break
 				}
-			}
+			}else{
+                switch(_.keyCode){
+                    case 8:
+                        if(his.length>0&&input.selectionEnd==input.selectionStart&&input.selectionEnd==0){
+                            hisix++
+                            if(hisix>his.length){
+                                hisix--
+                                input.value=""
+                            }else{
+                                input.value=his[his.length-hisix]
+                                input.selectionEnd=0
+                            }
+                        }else if(input.selectionEnd==input.selectionStart&&input.selectionEnd>0){
+                            input.value=input.value.substr(0,input.value.length-1)
+                        }
+						event.preventDefault()
+                        break
+                }
+            }
 			if(input.value==""){
 				if(his.length==0)return
 				switch(_.keyCode){
@@ -180,6 +205,13 @@ class con{
 		window.log=function(){
 			var line=dc("div")
 			line.textContent=[...arguments].join(" ")
+            line.onclick=_=>{
+                if(line.textContent.substr(0,2)=="> ")
+                    input.value=line.textContent.substr(2)
+                else
+                    input.value=line.textContent
+                input.focus()
+            }
 			wnd.appendChild(line)
 			autohide()
 			wnd.lastElementChild.scrollIntoView()
